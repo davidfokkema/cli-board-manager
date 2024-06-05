dc6fd2591d80f6c250f5381427c63e2d6d66d46a
David Fokkema <davidfokkema@icloud.com>
2024-06-05 14:59:20 +0200

    Select items to copy contents to clipboard

--- src/cli_board_manager/app.py
+++ src/cli_board_manager/app.py
@@ -14,27 +14,55 @@ class ClipItem(ListItem):
 
 
 class ClipBoard(ListView):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    BINDINGS = [("c", "clear", "Clear")]
+
+    current_item: ClipItem | None = None
 
     def on_mount(self) -> None:
         self.set_interval(0.2, self.watch_clipboard)
 
     def watch_clipboard(self) -> None:
         contents = pyperclip.paste()
-        if contents is not None:
-            if not self.children or contents != self.children[-1].contents:
-                self.append(ClipItem(contents=contents))
+        if contents != self.current_contents():
+            self.append(item := ClipItem(contents=contents))
+            self.set_current_item(item)
+
+    def current_contents(self) -> str:
+        if self.current_item:
+            return self.current_item.contents
+        else:
+            return ""
+
+    @on(ListView.Selected)
+    def copy_contents(self, event: ListView.Selected) -> None:
+        self.set_current_item(event.item)
+        pyperclip.copy(self.current_item.contents)
+        self.notify("Copied contents to clipboard.")
+
+    def set_current_item(self, item):
+        if self.current_item:
+            self.current_item.remove_class("current")
+        item.add_class("current")
+        # self.parent.scroll_visible(item)
+        self.current_item = item
+
+    def action_clear(self) -> None:
+        self.clear()
 
 
 class CliBoardManagerApp(App[None]):
     CSS_PATH = "app.tcss"
+
+    BINDINGS = [("q", "quit", "Quit")]
 
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
         with VerticalScroll():
             yield ClipBoard(id="clipboard")
+
+    def action_quit(self):
+        self.exit()
 
 
 if __name__ == "__main__":
